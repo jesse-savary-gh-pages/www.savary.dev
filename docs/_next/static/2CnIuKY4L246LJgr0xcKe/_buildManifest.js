bfaf2c0e950636b2a4ff8413f0c92d5731349daa
Jesse Savary <jesse-savary@users.noreply.github.com>
2020-10-17 15:18:45 -0300

    rebuild website, prune older builds

---
+++ docs/_next/static/2CnIuKY4L246LJgr0xcKe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fb7ca7ca61587ad4c9ae1.css","static\u002Fchunks\u002Fpages\u002Findex-f2d4c0b1d0e88c75f792.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
